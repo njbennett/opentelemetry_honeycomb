eb1c161fb5d1ca56f7f8c9e523107be03752f8da
Garth Kidd <garth@amplified.ai>
2021-03-18 16:22:51 +1100

    Correct documentation for json_backend.

--- lib/open_telemetry/honeycomb/json.ex
+++ lib/open_telemetry/honeycomb/json.ex
@@ -5,18 +5,17 @@ defmodule OpenTelemetry.Honeycomb.Json do
   The OpenTelemetry Honeycomb Exporter uses a `Jason`-style JSON encoder via a behaviour so you
   can adapt it to your preferred JSON encoder or decode/encode options.
 
-  To use `Poison`, install it. The exporter defaults `json_backend` to
+  To use `Poison`, install it. The exporter defaults `json_module` to
   `OpenTelemetry.Honeycomb.Json.PoisonBackend`, which adapts `Poison` to the subset of the
   `Jason` API we document with this behaviour.
 
-  To use `Jason`, install it and configure it as the `json_backend`:
+  To use `Jason`, install it and configure it as the `json_module`:
 
   ```
   config :opentelemetry,
     processors: [
       otel_batch_processor: %{
-        exporter: OpenTelemetry.Honeycomb.Exporter,
-        json_backend: Jason
+        exporter: {OpenTelemetry.Honeycomb.Exporter, json_module: Jason}
       }
     ]
   ```
